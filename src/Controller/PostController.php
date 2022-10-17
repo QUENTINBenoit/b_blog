efc41745d979b0a58506e3f40077d83a23da8b82
github_QUENTINBenoit <benquelm@gmail.com>
2022-10-17 16:05:20 +0200

    mise en place d'une pagination v-1

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -3,17 +3,25 @@
 namespace App\Controller;
 
 use App\Repository\PostRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class PostController extends AbstractController
 {
     #[Route('/post', name: 'list_post', methods: ['GET'] )]
-    public function listAllPost(PostRepository $postRepository): Response
+    public function listAllPost(PostRepository $postRepository,  PaginatorInterface $paginatorInterface, Request $request): Response
     {
             $postList = $postRepository->findAll(); 
-            $postPublished = $postRepository->findPosPublished(); 
+            $datas= $postRepository->findPosPublished();
+            $postPublished = $paginatorInterface->paginate(
+                $datas,
+                $request->query->getInt('page',  1),
+                4               
+            );           
             \dump($postPublished);
  
         return $this->render('post/index.html.twig', [
@@ -22,13 +30,4 @@ class PostController extends AbstractController
         ]);
     }
 
-    // #[Route('/postpub', name: 'pub_post', methods: ['GET'] )]
-    // public function publishedPost(PostRepository $postRepository): Response
-    // {
-    //         $postPublished = $postRepository->findPosPublished(); 
- 
-    //     return $this->render('post/index.html.twig', [
-    //         'postPublished' =>  $postPublished,
-    //     ]);
-    // }
 }
